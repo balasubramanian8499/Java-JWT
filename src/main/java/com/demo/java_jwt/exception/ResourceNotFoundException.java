6d4a7dc7cf9984cb600249f41e4798947e6473d4
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:34:25 +0530

    Adding src/main/java/com/demo/java_jwt/exception/ResourceNotFoundException.java

---
+++ src/main/java/com/demo/java_jwt/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.demo.java_jwt.exception;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+
+
+
+}
