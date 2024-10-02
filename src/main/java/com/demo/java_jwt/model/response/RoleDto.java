0f1e63a028aee61a9e1064efcc3c860be385114e
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:34:32 +0530

    Adding src/main/java/com/demo/java_jwt/model/response/RoleDto.java

---
+++ src/main/java/com/demo/java_jwt/model/response/RoleDto.java
@@ -0,0 +1,12 @@
+package com.demo.java_jwt.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoleDto {
+
+    private Integer roleId;
+    private String roleName;
+}
